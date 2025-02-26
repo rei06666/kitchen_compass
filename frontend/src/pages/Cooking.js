fe6022c0470d2b920c47de3abee679a6d0357430
rei06666 <naruserei@icloud.com>
2025-02-27 08:38:33 +0900

    メニューカードの追加

--- frontend/src/pages/Cooking.js
+++ frontend/src/pages/Cooking.js
@@ -5,6 +5,187 @@ import Underbar from '../component/Underbar';
 import app_logo from "../image/kitchen_compass_logo.png";
 import logout_logo from "../image/logout.png";
 
+const menuData = {
+    menus: [
+        {
+            "name": "ハンバーグ" ,
+            "image": "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG91D0jtYA8ffcvNYEaSWqFiu6NEj1MaTdZ-CPt-6YVVlxyY7tEABkq5AmX6qnvqGZrY-UPo52uyglfeLvy2mDvmPcOnrgKaDPZK6tNNjRL1saE4-W9uPEB76D8ZD-3inzOxrYG5tFG5kVMAHJXNVkDgUUjQfrSz3EoTjCHnQc5VLy081uZa8lDuwdcy3vRMShigbDBCLSj4c9RJD2XYiO1uYhye2oJxDZ2eIIyo2EvVZBj2JTHC0INSvFj4nRO82Ln8FgJwley2l1ExQSlWmQ-8=/hanba-gu6321.JPG",
+            "description": "ハンバーグの説明",
+            "ingredients": [
+                {
+                    "name": "挽き肉",
+                    "quantity": "200g"
+                },
+                {
+                    "name": "玉ねぎ",
+                    "quantity": "1/2個"
+                },
+                {
+                    "name": "パン粉",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "牛乳",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "塩",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "こしょう",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "卵",
+                    "quantity": "1個"
+                },
+                {
+                    "name": "ケチャップ",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "ソース",
+                    "quantity": "適量"
+                }
+            ],
+            "url": "https://www.kurashiru.com/recipes/d70b6d20-e200-460e-8a98-407117ab8724"
+        },
+        {
+            "name": "ハンバーグ" ,
+            "image": "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG91D0jtYA8ffcvNYEaSWqFiu6NEj1MaTdZ-CPt-6YVVlxyY7tEABkq5AmX6qnvqGZrY-UPo52uyglfeLvy2mDvmPcOnrgKaDPZK6tNNjRL1saE4-W9uPEB76D8ZD-3inzOxrYG5tFG5kVMAHJXNVkDgUUjQfrSz3EoTjCHnQc5VLy081uZa8lDuwdcy3vRMShigbDBCLSj4c9RJD2XYiO1uYhye2oJxDZ2eIIyo2EvVZBj2JTHC0INSvFj4nRO82Ln8FgJwley2l1ExQSlWmQ-8=/hanba-gu6321.JPG",
+            "description": "ハンバーグの説明",
+            "ingredients": [
+                {
+                    "name": "挽き肉",
+                    "quantity": "200g"
+                },
+                {
+                    "name": "玉ねぎ",
+                    "quantity": "1/2個"
+                },
+                {
+                    "name": "パン粉",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "牛乳",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "塩",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "こしょう",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "卵",
+                    "quantity": "1個"
+                },
+                {
+                    "name": "ケチャップ",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "ソース",
+                    "quantity": "適量"
+                }
+            ],
+            "url": "https://www.kurashiru.com/recipes/d70b6d20-e200-460e-8a98-407117ab8724"
+        },
+        {
+            "name": "ハンバーグ" ,
+            "image": "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG91D0jtYA8ffcvNYEaSWqFiu6NEj1MaTdZ-CPt-6YVVlxyY7tEABkq5AmX6qnvqGZrY-UPo52uyglfeLvy2mDvmPcOnrgKaDPZK6tNNjRL1saE4-W9uPEB76D8ZD-3inzOxrYG5tFG5kVMAHJXNVkDgUUjQfrSz3EoTjCHnQc5VLy081uZa8lDuwdcy3vRMShigbDBCLSj4c9RJD2XYiO1uYhye2oJxDZ2eIIyo2EvVZBj2JTHC0INSvFj4nRO82Ln8FgJwley2l1ExQSlWmQ-8=/hanba-gu6321.JPG",
+            "description": "ハンバーグの説明",
+            "ingredients": [
+                {
+                    "name": "挽き肉",
+                    "quantity": "200g"
+                },
+                {
+                    "name": "玉ねぎ",
+                    "quantity": "1/2個"
+                },
+                {
+                    "name": "パン粉",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "牛乳",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "塩",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "こしょう",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "卵",
+                    "quantity": "1個"
+                },
+                {
+                    "name": "ケチャップ",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "ソース",
+                    "quantity": "適量"
+                }
+            ],
+            "url": "https://www.kurashiru.com/recipes/d70b6d20-e200-460e-8a98-407117ab8724"
+        },
+        {
+            "name": "ハンバーグ" ,
+            "image": "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG91D0jtYA8ffcvNYEaSWqFiu6NEj1MaTdZ-CPt-6YVVlxyY7tEABkq5AmX6qnvqGZrY-UPo52uyglfeLvy2mDvmPcOnrgKaDPZK6tNNjRL1saE4-W9uPEB76D8ZD-3inzOxrYG5tFG5kVMAHJXNVkDgUUjQfrSz3EoTjCHnQc5VLy081uZa8lDuwdcy3vRMShigbDBCLSj4c9RJD2XYiO1uYhye2oJxDZ2eIIyo2EvVZBj2JTHC0INSvFj4nRO82Ln8FgJwley2l1ExQSlWmQ-8=/hanba-gu6321.JPG",
+            "description": "ハンバーグの説明",
+            "ingredients": [
+                {
+                    "name": "挽き肉",
+                    "quantity": "200g"
+                },
+                {
+                    "name": "玉ねぎ",
+                    "quantity": "1/2個"
+                },
+                {
+                    "name": "パン粉",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "牛乳",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "塩",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "こしょう",
+                    "quantity": "少々"
+                },
+                {
+                    "name": "卵",
+                    "quantity": "1個"
+                },
+                {
+                    "name": "ケチャップ",
+                    "quantity": "大さじ1"
+                },
+                {
+                    "name": "ソース",
+                    "quantity": "適量"
+                }
+            ],
+            "url": "https://www.kurashiru.com/recipes/d70b6d20-e200-460e-8a98-407117ab8724"
+        }
+    ]
+}
+
 const Cooking = () => {
     const navigate = useNavigate();
     const location = useLocation();
@@ -71,6 +252,31 @@ const Cooking = () => {
                                 <option value="AnyIngredient">Any ingredient</option>
                             </select>
                         </div>
+                    </div>
+                    <div className="rounded-xl p-5 border-gray-900 shadow w-full mt-[5%]">
+                        <form class="w-full">
+                        <label for="message" class="w-full mb-2 text-lg font-medium dark:text-black">What kind of dish do you want to eat?</label>
+                            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Feel free to write your request"></textarea>
+                        </form>
+                        <div class="flex justify-end">
+                            <button class="text-lg right-2 mt-5 bg-orange-400 hover:bg-orange-500 border-gray-500 border text-white font-bold py-2 px-4 rounded">Request</button>
+                        </div>
+                    </div>
+                    <h2 className="mt-20 font-bold text-orange-950">menu</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-5">
+                        {menuData.menus.map((menu, index) => (
+                            <div key={index} className="border-3  rounded-lg p-5 shadow  bg-orange-200 hover:bg-orange-300">
+                                <h3 className="mt-2 mb-2 text-xl font-bold">{menu.name}</h3>
+                                <img src={menu.image} alt={menu.name} className="w-full object-cover rounded-lg" />
+                                <p className="mt-1 text-gray-700">{menu.description}</p>
+                                <ul className="mt-2">
+                                    {menu.ingredients.map((ingredient, idx) => (
+                                        <li key={idx} className="text-gray-600">{ingredient.name}: {ingredient.quantity}</li>
+                                    ))}
+                                </ul>
+                                <a href={menu.url} target="_blank" rel="noopener noreferrer" className="mt-4 inline-block bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded">View Recipe</a>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
